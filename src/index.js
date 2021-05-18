c4bededa4b9d529109a35a12c8aaef054a1c598e
Samuel Swank <samswank@gmail.com>
2021-05-18 15:57:30 -0600

    preliminary display

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/header'
+import RightPanel from './components/rightPanel'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <RightPanel />
   </React.StrictMode>,
   document.getElementById('root')
 );
