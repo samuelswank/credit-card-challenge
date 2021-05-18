7d7fdac6fce48af960432bc543942b5a3ed5e4cd
Samuel Swank <samswank@gmail.com>
2021-05-18 15:57:11 -0600

    header and panel components + json

---
+++ src/components/header.jsx
@@ -0,0 +1,24 @@
+import React, { Component, Fragment } from 'react';
+import { Divider } from 'antd';
+
+class Header extends Component {
+    styles = {
+        'h3-text': { marginBottom: '5px' },
+        'Divider': { borderTop: '1px solid gray' }
+    }
+    render() { 
+        return (
+        <div>
+            <h3 style = {this.styles['h3-text']}>Your Card</h3>
+            <text style={this.styles['h3-text']}>
+                Use this card to pay for programs in your plan.
+                Purchases will be visible to your manager.
+            </text>
+            <br /><br />
+            <Divider style={this.styles['Divider']} />
+        </div>
+        );
+    }
+}
+ 
+export default Header;
